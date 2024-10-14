7b263e353f0af691e6d8ef5679f5dd7c6606efe8
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-14 14:58:21 -0300

    feat: added user image and description for user

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,21 +4,33 @@ import './styles.css'
 
 function App() {
   return (
-  <>
-    <Header/>
-    
-    <div className="conteudo">
-      <img src={background} className="background" alt='Imagem do GitHub'/>
+    <>
+      <Header />
 
-    <div className="info">
-      <div>
-        <input name="usuario" placeholder="@username"/>
-        <button>Buscar</button>
+      <div className="conteudo">
+        <img src={background} className="background" alt='Imagem do GitHub' />
+
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+          </div>
+          <div className="perfil" >
+            <img
+              src="https://media.licdn.com/dms/image/v2/D4D35AQHyp10n1FNY4w/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1710607237792?e=1729533600&v=beta&t=gTeivW9Oi5vXqVuJV53Ow0Mk2fqaCLk2PCHsq8VK2X8"
+              className="profile"
+              alt="imagem de perfil"
+            />
+            <div>
+              <h3>Edvaldo Vitor</h3>
+              <p>Desenvolvedor Web</p>
+              <span>Desenvolvedor Web</span>
+            </div>
+          </div>
+          <hr/>
+        </div>
       </div>
-    </div>
-
-    </div>
-  </>
+    </>
   );
 }
 
