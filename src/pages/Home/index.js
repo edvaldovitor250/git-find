10725c9599cdee727ab3901e577d5a93cc981087
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-14 12:04:34 -0300

    feat: create button and input is styled

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,24 @@
 import { Header } from "../../components/Header";
+import background from '../../assests/background.png'
+import './styles.css'
 
 function App() {
   return (
+  <>
     <Header/>
+    
+    <div className="conteudo">
+      <img src={background} className="background" alt='Imagem do GitHub'/>
+
+    <div className="info">
+      <div>
+        <input name="usuario" placeholder="@username"/>
+        <button>Buscar</button>
+      </div>
+    </div>
+
+    </div>
+  </>
   );
 }
 
