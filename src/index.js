510bc607a8822be57e22dad96b347e864fbb4f09
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-22 14:14:55 -0300

    feat: added StyledComponent in project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css'
+import './global-styles.css'
 import App from './pages/Home';
+import { StylesGlobals } from './styles.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StylesGlobals/>
     <App />
   </React.StrictMode>
 );
---
+++ src/styles.js
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line no-undef
+import styled from 'styled-components';
+import './global-styles.css'
+
+export const StylesGlobals = styled.div` 
+* {
+    margin: 0;
+    padding: 0;
+}
+`;
